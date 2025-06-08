9e5f3b2cfbe8a5c749cb9c18aceec5e2dcdf9bdc
azeemkhan <azeemkh528@gmail.com>
2025-06-08 15:45:08 +0530

    updated here video for optimization

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,16 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/blogs/[slug]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/blogs/[slug].js"
-    ],
-    "/services/[slug]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/services/[slug].js"
     ]
   },
   "ampFirstPages": []
